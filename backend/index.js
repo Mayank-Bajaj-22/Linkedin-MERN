735dca34d51979c25d8efddc89f9b9b6d8b32353
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-01-17 07:49:48 +0530

    db connected and user model

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./config/db.js";
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`Server running on ${port}: http://localhost:${port}/`)
+})
