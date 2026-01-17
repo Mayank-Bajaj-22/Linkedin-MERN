f765109d4fe4a141ef695b87c2e1966d2cd93def
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-01-17 19:23:29 +0530

    sign up, axios, and other things done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,11 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 const port = process.env.PORT || 3000;
 app.use("/api/auth", authRouter)
