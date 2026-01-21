2137f314630de77b372a63b8e9e31943eeb9def3
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-01-21 19:20:01 +0530

    add like and comment functionality to posts; update routes accordingly

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -35,3 +35,47 @@ export const getPost = async (req, res) => {
         return res.status(500).json({ message: "getPost error"})
     }
 }
+export const like = async (req, res) => {
+    try {
+        let postId = req.params.id
+        let userId = req.userId
+
+        let post = await Post.findById(postId)
+
+        if (!post) {
+            return res.status(400).json({ message: "post not found" })
+        }
+
+        if (post.like.includes(userId)) {
+            post.like.filter((id) => id != userId)
+        } else {
+            post.like.push(userId)
+        }
+
+        await post.save()
+
+        return res.status(200).json(post)
+
+    } catch (error) {
+        return res.status(500).json({ message: `like error ${error}` })
+    }
+}
+
+export const comment = async (req, res) => {
+    try {
+        let postId = req.params.id
+        let userId = req.userId
+
+        let { content } = req.body;
+
+        let post = await Post.findByIdAndUpdate(postId, {
+            $push: { comment: {content, user: userId} }
+        }, { new: true })
+        .populate("comment.user", "firstName lastName profileImage headline")
+
+        return res.status(200).json(post)
+
+    } catch (error) {
+        return res.status(500).json({ message: `comment error ${error}` })
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
 import upload from "../middlewares/multer.js";
-import { createPost, getPost } from "../controllers/post.controllers.js";
+import { comment, createPost, getPost, like } from "../controllers/post.controllers.js";
 
 let postRouter = express.Router();
 
 postRouter.post("/createpost", isAuth, upload.single("image"), createPost)
 postRouter.get("/getpost", isAuth, getPost)
+postRouter.get("/like/:id", isAuth, like)
+postRouter.post("/comment/:id", isAuth, comment)
+
+// :id called params
 
 export default postRouter;
